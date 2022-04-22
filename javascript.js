cb87a0fb92ffef1db61d31839ac94a3fe93a56d2
Samson Fasuji <64075566+Fash-me@users.noreply.github.com>
2022-04-22 04:02:46 -0700

    Add files via upload
    Initial commit

--- javascript.js
+++ javascript.js
@@ -1,1 +1,28 @@
-console.log("Hello World blaaaaaaaaa");
+let userPlay = window.prompt("Rock, Paper or Scissors?");
+console.log(userPlay); // The user will input their selection and it will display the user input.
+
+let word = ['Rock', 'Paper', 'Scissors']; //The list of variable the computer will select from.
+
+    let result = word[Math.floor(Math.random()*word.length)];
+    console.log(result); // This is the computer selection
+    
+    function playRound(playerSelection, computerSelection) {
+        playerSelection = userPlay.toLowerCase();
+        computerSelection = result.toLowerCase();
+        if ((playerSelection == 'rock' && computerSelection == 'scissors') || 
+        (playerSelection == 'scissors' && computerSelection == 'paper') || 
+        (playerSelection == 'paper' && computerSelection == 'rock')) {
+            console.log("You won! " + playerSelection + ' beats ' + computerSelection);
+        } else if ((computerSelection == 'rock' && playerSelection == 'scissors') || 
+        (computerSelection == 'scissors' && playerSelection == 'paper') || 
+        (computerSelection == 'paper' && playerSelection == 'rock')) {
+            console.log("You lose! " + computerSelection + ' beats ' + playerSelection)
+        } else if (computerSelection == playerSelection) {
+            console.log("Tie game.")
+        }
+        else {
+            console.log(userPlay + " is invalid. Input Rock, Paper or scissors")
+        }
+    };
+    
+    playRound(result, userPlay);
