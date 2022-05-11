e7ec78aa6797f780859d0a302f21ec909e13f7d3
Samson Fasuji <64075566+Fash-me@users.noreply.github.com>
2022-05-11 21:41:12 +0800

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,28 +1,34 @@
-let userPlay = window.prompt("Rock, Paper or Scissors?");
-console.log(userPlay); // The user will input their selection and it will display the user input.
+let userPlay = window.prompt("Rock, Paper or Scissors?");// The user will input their selection and it will display the user input.
 
 let word = ['Rock', 'Paper', 'Scissors']; //The list of variable the computer will select from.
 
-    let result = word[Math.floor(Math.random()*word.length)];
-    console.log(result); // This is the computer selection
-    
+function computerPlay(){
+    return word[Math.floor(Math.random()*word.length)];// This is the computer selection
+}
     function playRound(playerSelection, computerSelection) {
-        playerSelection = userPlay.toLowerCase();
-        computerSelection = result.toLowerCase();
         if ((playerSelection == 'rock' && computerSelection == 'scissors') || 
         (playerSelection == 'scissors' && computerSelection == 'paper') || 
         (playerSelection == 'paper' && computerSelection == 'rock')) {
-            console.log("You won! " + playerSelection + ' beats ' + computerSelection);
+            return "You won! " + playerSelection + ' beats ' + computerSelection;
         } else if ((computerSelection == 'rock' && playerSelection == 'scissors') || 
         (computerSelection == 'scissors' && playerSelection == 'paper') || 
         (computerSelection == 'paper' && playerSelection == 'rock')) {
-            console.log("You lose! " + computerSelection + ' beats ' + playerSelection)
+            return "You lose! " + computerSelection + ' beats ' + playerSelection
         } else if (computerSelection == playerSelection) {
-            console.log("Tie game.")
+            return "Tie game."
         }
         else {
-            console.log(userPlay + " is invalid. Input Rock, Paper or scissors")
+            return userPlay + " is invalid. Input Rock, Paper or scissors"
         }
     };
-    
-    playRound(result, userPlay);
+
+    playerSelection = userPlay.toLowerCase();
+    computerSelection = computerPlay().toLowerCase();
+    console.log(playRound(playerSelection, computerSelection));
+
+function game(){
+    playRound(playerSelection, computerSelection)
+    for (let i = 0; i < 5; i++){
+        
+    }
+}
