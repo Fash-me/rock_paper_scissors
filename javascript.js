1ff028b4af8233b06be7e948c061f0f49baee115
Samson Fasuji <64075566+Fash-me@users.noreply.github.com>
2022-05-12 22:41:37 +0800

    Initial commit

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-let userPlay = window.prompt("Rock, Paper or Scissors?");// The user will input their selection and it will display the user input.
+let userName = window.prompt("What is your name?") // The user will input a name.
 
 let word = ['Rock', 'Paper', 'Scissors']; //The list of variable the computer will select from.
 
@@ -9,10 +9,12 @@ function computerPlay(){
         if ((playerSelection == 'rock' && computerSelection == 'scissors') || 
         (playerSelection == 'scissors' && computerSelection == 'paper') || 
         (playerSelection == 'paper' && computerSelection == 'rock')) {
+            playerScore++;
             return "You won! " + playerSelection + ' beats ' + computerSelection;
         } else if ((computerSelection == 'rock' && playerSelection == 'scissors') || 
         (computerSelection == 'scissors' && playerSelection == 'paper') || 
         (computerSelection == 'paper' && playerSelection == 'rock')) {
+            computerScore++;
             return "You lose! " + computerSelection + ' beats ' + playerSelection
         } else if (computerSelection == playerSelection) {
             return "Tie game."
@@ -22,13 +24,26 @@ function computerPlay(){
         }
     };
 
-    playerSelection = userPlay.toLowerCase();
+    playerSelection = ""
     computerSelection = computerPlay().toLowerCase();
-    console.log(playRound(playerSelection, computerSelection));
+    playerScore = 0;
+    computerScore = 0;
 
 function game(){
-    playRound(playerSelection, computerSelection)
     for (let i = 0; i < 5; i++){
-        
+        playerSelection = window.prompt("Rock, Paper or Scissors?");// The user will input their selection and it will display the user input.
+
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(playerScore);
+        console.log(computerScore);
+    }
+    if (computerScore > playerScore) {
+        alert("Computer won " + computerScore + " - " + playerScore)
+    } else if (playerScore > computerScore) {
+        alert(userName + " won " + playerScore + " - " + computerScore)
+    }
+    else {
+        alert("Better luck next time, is is tie" + playerScore + " - " + computerScore)
     }
 }
+game();
