b240d6039975d688626a1c5dc5651e58bcf48d9b
Samson Fasuji <64075566+Fash-me@users.noreply.github.com>
2022-05-13 21:48:29 +0800

    Initial commit

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ let userName = window.prompt("What is your name?") // The user will input a name
 
 let word = ['Rock', 'Paper', 'Scissors']; //The list of variable the computer will select from.
 
-function computerPlay(){
+function computerPlay() {
     return word[Math.floor(Math.random()*word.length)];// This is the computer selection
 }
     function playRound(playerSelection, computerSelection) {
@@ -20,19 +20,17 @@ function computerPlay(){
             return "Tie game."
         }
         else {
-            return userPlay + " is invalid. Input Rock, Paper or scissors"
+             console.log(playerSelection + " is invalid. Input Rock, Paper or scissors")
         }
-    };
+    }; //This function will decide who wins in each rounds.
 
-    playerSelection = ""
-    computerSelection = computerPlay().toLowerCase();
     playerScore = 0;
     computerScore = 0;
 
 function game(){
     for (let i = 0; i < 5; i++){
-        playerSelection = window.prompt("Rock, Paper or Scissors?");// The user will input their selection and it will display the user input.
-
+        playerSelection = window.prompt("Rock, Paper or Scissors?").toLowerCase();// The user will input their selection and it will display the user input.
+        computerSelection = computerPlay().toLowerCase();
         console.log(playRound(playerSelection, computerSelection));
         console.log(playerScore);
         console.log(computerScore);
@@ -43,7 +41,7 @@ function game(){
         alert(userName + " won " + playerScore + " - " + computerScore)
     }
     else {
-        alert("Better luck next time, is is tie" + playerScore + " - " + computerScore)
+        alert("Better luck next time, it is a  tie game " + playerScore + " - " + computerScore)
     }
-}
+} //This function will play 5 rounds and determine the winner
 game();
