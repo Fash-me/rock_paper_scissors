203c95c224750bbf2677ddbed66e3f7cfc854011
Fash-me <fasujitosin@gmail.com>
2023-04-07 20:12:39 +0100

    GUI included

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rock_paper_scissors
-rock, paper, scissors
+This is a rock paper scissors game project from TheOdinProject. This is a nice project that made me conbined the HTML, CSS, and JavaScript skills to form this lovely wabpage. Tell me what you think about this projoct. Here are some screenshots of the game.
+![Screenshot](images/Screen Shot 2023-04-07 at 8.06.51 PM.png)
+![Screenshot](images/Screen Shot 2023-04-07 at 8.07.13 PM.png)
+![Screenshot](images/Screen Shot 2023-04-07 at 8.07.32 PM.png)
+![Screenshot](images/Screen Shot 2023-04-07 at 8.07.53 PM.png)
--- app.js
+++ app.js
@@ -1,47 +1,96 @@
-let userName = window.prompt("What is your name?").toUpperCase() // The user will input a name.
+let userName;
+let word;
+let playerScore;
+let computerScore;
 
-let word = ['Rock', 'Paper', 'Scissors']; //The list of variable the computer will select from.
+
+userName = window.prompt("What is your name?").toUpperCase() // The user will input a name.
+
+const wcPlayer = document.querySelector('#wc-Player');
+const restartGame = document.querySelector('#restart-game');
+const nextPlayer = document.querySelector('#next-player');
+const declareWinner = document.querySelector('#declearin-winner')
+const showPopUp = document.querySelector("#pop-up")
+const rockBtn = document.querySelector('#rock-btn');
+const paperBtn = document.querySelector('#paper-btn');
+const scissorsBtn = document.querySelector('#scissors-btn');
+
+wcPlayer.innerText = `WELCOME ${userName} `;
+
+word = ['Rock', 'Paper', 'Scissors']; //The list of variable the computer will select from.
 
 function computerPlay() {
     return word[Math.floor(Math.random()*word.length)];// This is the computer selection
 }
-    function playRound(playerSelection, computerSelection) {
-        if ((playerSelection == 'rock' && computerSelection == 'scissors') || 
-        (playerSelection == 'scissors' && computerSelection == 'paper') || 
-        (playerSelection == 'paper' && computerSelection == 'rock')) {
-            playerScore++;
-            return "You won! " + playerSelection + ' beats ' + computerSelection;
-        } else if ((computerSelection == 'rock' && playerSelection == 'scissors') || 
-        (computerSelection == 'scissors' && playerSelection == 'paper') || 
-        (computerSelection == 'paper' && playerSelection == 'rock')) {
-            computerScore++;
-            return "You lose! " + computerSelection + ' beats ' + playerSelection
-        } else if (computerSelection == playerSelection) {
-            return "Tie game."
-        }
-        else {
-             console.log(playerSelection + " is invalid. Input Rock, Paper or scissors")
-        }
-    }; //This function will decide who wins in each rounds.
 
-    playerScore = 0;
-    computerScore = 0;
+playerScore = 0;
+computerScore = 0;
 
-function game(){
-    for (let i = 0; i < 5; i++){
-        playerSelection = window.prompt("Rock, Paper or Scissors?").toLowerCase();// The user will input their selection and it will display the user input.
-        computerSelection = computerPlay().toLowerCase();
-        console.log(playRound(playerSelection, computerSelection));
-        console.log(playerScore);
-        console.log(computerScore);
+restartGame.addEventListener("click", restart);
+function restart() {
+    window.location.reload();
+}
+
+nextPlayer.addEventListener("click", restart);
+function restart() {
+    window.location.reload();
+} // This function will restart the game at any point
+
+function playerPopUp() {
+    declareWinner.innerText = `Alright, ${userName} you are my master. You won the game but that won't stop me from taking over the world ha-ha`
+    showPopUp.showModal();
+
+}// //This function will show and display player message in the pop-up
+
+function computerPopUp() {
+    declareWinner.innerText = `I told you I was going to take over the world. Now I will start by defeating every player in this game ha-ha`
+    showPopUp.showModal();
+
+}; //This function will show and display computer message in the pop-up
+
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') || 
+    (playerSelection == 'scissors' && computerSelection == 'paper') || 
+    (playerSelection == 'paper' && computerSelection == 'rock')) {
+        playerScore++;
+        document.querySelector('#comment').innerText = `You won this round ${userName}, the next round will be mine.`;
+    } else if ((computerSelection == 'rock' && playerSelection == 'scissors') || 
+    (computerSelection == 'scissors' && playerSelection == 'paper') || 
+    (computerSelection == 'paper' && playerSelection == 'rock')) {
+        computerScore++;
+        document.querySelector('#comment').innerText = `I won this round, I am about to take over the world.`;
+    } else {
+        document.querySelector('#comment').innerText = "It is tie, you think you are tough ehn";
     }
-    if (computerScore > playerScore) {
-        alert("COMPUTER won " + computerScore + " - " + playerScore)
-    } else if (playerScore > computerScore) {
-        alert(userName + " won " + playerScore + " - " + computerScore)
+
+    if (playerScore == 5) {
+        playerPopUp()
+    } else if (computerScore == 5) {
+        computerPopUp()
     }
-    else {
-        alert("Better luck next time " + userName + ", it is a  tie game " + playerScore + " - " + computerScore)
-    };
-};//This function will play 5 rounds and determine the winner
-game();
+
+    document.querySelector('#com-play').innerText = `Computer played ${computerSelection}`;
+    document.querySelector('#ply-score').innerText = `${userName} SCORE =  ${playerScore}`;
+    document.querySelector('#com-score').innerText = `COMPUTER SCORE =  ${computerScore}`;
+}; //This function will decide who wins in each rounds and declare the winner.
+
+rockBtn.addEventListener("click", rock);
+function rock() {
+    playerSelection = "rock";
+    computerSelection = computerPlay().toLowerCase();
+    playRound(playerSelection, computerSelection);
+}; //The function for the rock click
+
+paperBtn.addEventListener("click", paper);
+function paper(playerSelection, computerSelection) {
+    playerSelection = "paper"
+    computerSelection = computerPlay().toLowerCase();
+    playRound(playerSelection, computerSelection);
+}; //The function for the paper click
+
+scissorsBtn.addEventListener("click", scissorsPlayRound);
+function scissorsPlayRound(playerSelection, computerSelection) {
+    playerSelection = "scissors"
+    computerSelection = computerPlay().toLowerCase();
+    playRound(playerSelection, computerSelection);
+}; //The function for the scissors click
